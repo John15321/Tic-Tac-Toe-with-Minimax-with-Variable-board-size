aa5c01794f294d473f47c8a9ab05e6d85fb25f4f
John15321 <johngadsden1775@gmail.com>
2020-05-15 09:54:06 +0200

    Buttons work, colors picked, icons picked

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ class Button:
     Parent Button class
     '''
 
-    def __init__(self, x_position, y_position,  width, height, button_color):
+    def __init__(self, x_position, y_position,  width, height, button_color, hover_color):
         self.x_position = x_position
         self.y_position = y_position
         self.width = width
         self.height = height
         self.button_color = button_color
+        self.hover_color = hover_color
 
 
 class ButtonWithText(Button):
@@ -24,9 +25,9 @@ class ButtonWithText(Button):
     A class for text that is contained inside buttons
     '''
 
-    def __init__(self, x_position, y_position,  width, height, button_color, text, text_color=(0, 0, 0), padding_x=0, padding_y=0):
+    def __init__(self, x_position, y_position,  width, height, button_color, hover_color, text, text_color=(0, 0, 0), padding_x=0, padding_y=0):
         super().__init__(x_position, y_position,
-                         width, height, button_color)
+                         width, height, button_color, hover_color)
         self.text = text
         self.text_color = text_color
         self.font = pygame.font.Font('freesansbold.ttf', 16)
@@ -43,25 +44,57 @@ class ButtonWithText(Button):
         screen.blit(self.text_render, (self.x_position +
                                        self.padding_x, self.y_position+self.padding_y))
 
+    def is_hovered(self):
+        '''
+        Draws the button with a different color as background when hovered over
+        '''
+        pygame.draw.rect(screen, self.hover_color, (self.x_position,
+                                                     self.y_position, self.width, self.height))
+        screen.blit(self.text_render, (self.x_position +
+                                       self.padding_x, self.y_position+self.padding_y))
+
+    def is_over(self, mouse_pos):
+        '''
+        Function return boolean of if the mouse while being clicked is over the particular button
+        '''
+        if mouse_pos[0] > self.x_position and mouse_pos[0] < self.x_position + self.width:
+            if mouse_pos[1] > self.y_position and mouse_pos[1] < self.y_position + self.height:
+                return True
+        else:
+            return False
+
+    def is_clicked(self):
+        print("Clicked")
+
 
 class ButtonWithImage(Button):
     '''
     A class for text that is contained inside buttons
     '''
 
-    def __init__(self, x_position, y_position,  width, height, button_color, button_image, padding_x=0, padding_y=0):
+    def __init__(self, x_position, y_position,  width, height, button_color, hover_color, button_image, padding_x=0, padding_y=0):
         super().__init__(x_position, y_position,
-                         width, height, button_color)
+                         width, height, button_color, hover_color)
         self.button_image = pygame.image.load(button_image)
         self.padding_x = padding_x
         self.padding_y = padding_y
 
-    def show_button(self):
+    def show_button(self, hover_color=(-1, -1, -1)):
         '''
         Function for showing a given button with its set positions and values, etc.
         '''
-        pygame.draw.rect(screen, self.button_color, (self.x_position,
-                                                     self.y_position, self.width, self.height))
+        if hover_color != (-1, -1, -1):
+            pygame.draw.rect(screen, hover_color, (self.x_position,
+                                                   self.y_position, self.width, self.height))
+        else:
+            pygame.draw.rect(screen, self.button_color, (self.x_position,
+                                                         self.y_position, self.width, self.height))
+        screen.blit(self.button_image, (self.x_position +
+                                        self.padding_x, self.y_position+self.padding_y))
+
+    def is_hovered(self):
+        pygame.draw.rect(screen, self.hover_color, (self.x_position,
+                                                    self.y_position, self.width, self.height))
         screen.blit(self.button_image, (self.x_position +
                                         self.padding_x, self.y_position+self.padding_y))
 
@@ -71,6 +104,19 @@ class ButtonWithImage(Button):
         '''
         self.button_image = pygame.image.load(new_image)
 
+    def is_over(self, mouse_pos):
+        '''
+        Function return boolean of if the mouse while being clicked is over the particular button
+        '''
+        if mouse_pos[0] > self.x_position and mouse_pos[0] < self.x_position + self.width:
+            if mouse_pos[1] > self.y_position and mouse_pos[1] < self.y_position + self.height:
+                return True
+        else:
+            return False
+
+    def is_clicked(self):
+        print("Clicked")
+
 
 # Creating the screen
 # screen size (has to be a tuple: width x height)
@@ -79,35 +125,43 @@ screen_height = 600
 screen_size = (screen_width, screen_height)
 screen = pygame.display.set_mode(screen_size)
 # screen color (in RGB)
-screen_color = (27, 27, 47)  # very darker blue/purple
+screen_color = (245, 167, 167)  # very darker blue/purple
+hover_color_for_buttons = (249, 216, 156)
+background_color_for_buttons = (130, 196, 195)
 
 # Title and Icon
 pygame.display.set_caption("Tic Tac Toe")
-# <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 pygame.display.set_icon(pygame.image.load("./Resources/game_icon.png"))
-# Circle and Cross icons:
-# <a target="_blank" href="https://icons8.com/icons/set/multiply">Multiply icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-# <a target="_blank" href="https://icons8.com/icons/set/minus">Minus icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-# <a target="_blank" href="https://icons8.com/icons/set/minus-math">Subtract icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-# <a target="_blank" href="https://icons8.com/icons/set/plus-math">Plus Math icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 
 
 def game_main_menu():
     MAIN_MENU = True
+    button = ButtonWithText(0, 0, 100, 50,
+                            background_color_for_buttons, hover_color_for_buttons, "TEST", (0, 0, 0), 10, 10)
+    button2 = ButtonWithImage(
+        100, 100, 64, 64, background_color_for_buttons, hover_color_for_buttons, "./Resources/cross_icon.png")
+    buttons = [button, button2]
 
     while MAIN_MENU:
+        screen.fill(screen_color)
         # for loop for catching evvents for PyGame
+        mouse_position = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 MAIN_MENU = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for each in buttons:
+                    if each.is_over(mouse_position):
+                        each.is_clicked()
 
-        screen.fill(screen_color)
-        button = ButtonWithText(0, 0, 100, 50,
-                                (255, 255, 255), "TEST", (0, 0, 0), 10, 10)
-        button.show_button()
+        # Checking if buttons are hovered over
+        # and if yes then change background button color
+        for each in buttons:
+            if each.is_over(mouse_position):
+                each.is_hovered()
+            else:
+                each.show_button()
 
-        button2 = ButtonWithImage(100, 100, 64, 64, screen_color, "./Resources/cross_icon.png")
-        button2.show_button()
         pygame.display.update()
 
 
