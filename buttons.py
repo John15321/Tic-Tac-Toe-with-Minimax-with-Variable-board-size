2839beb58263e44ca14eccbc58c3d8808bf4a7c3
BlueCookieFrog <35380525+BlueCookieFrog@users.noreply.github.com>
2020-05-15 19:49:39 +0200

    program splitted into modules

--- buttons.py
+++ buttons.py
@@ -1,14 +1,5 @@
-#!/usr/bin/python
 import pygame
-<<<<<<< Updated upstream
-
-# Initializing PyGame
-pygame.init()
-
-# screen color (in RGB)
-screen_color = (245, 167, 167)  # very darker blue/purple
-hover_color_for_buttons = (249, 216, 156)
-background_color_for_buttons = (130, 196, 195)
+from config import *
 
 
 class Button:
@@ -16,7 +7,7 @@ class Button:
     Parent Button class
     '''
 
-    def __init__(self, x, y,  width, height, button_color):
+    def __init__(self, x, y, width, height, button_color):
         self.x_position = x
         self.y_position = y
         self.width = width
@@ -30,7 +21,7 @@ class ButtonWithText(Button):
     A class for text that is contained inside buttons
     '''
 
-    def __init__(self, x, y,  width, height, button_color, text, text_color=(0, 0, 0), padding_x=0, padding_y=0):
+    def __init__(self, x, y, width, height, button_color, text, text_color=(0, 0, 0), padding_x=0, padding_y=0):
         super().__init__(x, y,
                          width, height, button_color)
         self.text = text
@@ -47,7 +38,7 @@ class ButtonWithText(Button):
         pygame.draw.rect(screen, self.button_color, (self.x_position,
                                                      self.y_position, self.width, self.height))
         screen.blit(self.text_render, (self.x_position +
-                                       self.padding_x, self.y_position+self.padding_y))
+                                       self.padding_x, self.y_position + self.padding_y))
 
     def is_hovered(self):
         '''
@@ -56,7 +47,7 @@ class ButtonWithText(Button):
         pygame.draw.rect(screen, self.hover_color, (self.x_position,
                                                     self.y_position, self.width, self.height))
         screen.blit(self.text_render, (self.x_position +
-                                       self.padding_x, self.y_position+self.padding_y))
+                                       self.padding_x, self.y_position + self.padding_y))
 
     def is_over(self, mouse_pos):
         '''
@@ -70,7 +61,9 @@ class ButtonWithText(Button):
 
     def is_clicked(self):
         print("Clicked")
-        screen = pygame.display.set_mode((900, 900))
+        screen = pygame.display.set_mode((500, 500))
+
+
 
 
 class ButtonWithImage(Button):
@@ -78,7 +71,7 @@ class ButtonWithImage(Button):
     A class for text that is contained inside buttons
     '''
 
-    def __init__(self, x, y,  width, height, button_color, button_image, padding_x=0, padding_y=0):
+    def __init__(self, x, y, width, height, button_color, button_image, padding_x=0, padding_y=0):
         super().__init__(x, y,
                          width, height, button_color)
         self.button_image = pygame.image.load(button_image)
@@ -96,13 +89,13 @@ class ButtonWithImage(Button):
             pygame.draw.rect(screen, self.button_color, (self.x_position,
                                                          self.y_position, self.width, self.height))
         screen.blit(self.button_image, (self.x_position +
-                                        self.padding_x, self.y_position+self.padding_y))
+                                        self.padding_x, self.y_position + self.padding_y))
 
     def is_hovered(self):
         pygame.draw.rect(screen, self.hover_color, (self.x_position,
                                                     self.y_position, self.width, self.height))
         screen.blit(self.button_image, (self.x_position +
-                                        self.padding_x, self.y_position+self.padding_y))
+                                        self.padding_x, self.y_position + self.padding_y))
 
     def change_image(self, new_image):
         '''
@@ -122,58 +115,4 @@ class ButtonWithImage(Button):
 
     def is_clicked(self):
         print("Clicked")
-        screen = pygame.display.set_mode((500, 500))
-
-
-# Creating the screen
-# screen size (has to be a tuple: width x height)
-screen_width = 800
-screen_height = 600
-screen_size = (screen_width, screen_height)
-screen = pygame.display.set_mode(screen_size)
-
-# Title and Icon
-pygame.display.set_caption("Tic Tac Toe")
-pygame.display.set_icon(pygame.image.load("./Resources/game_icon.png"))
-
-=======
-from config import *
-from buttons import *
->>>>>>> Stashed changes
-
-def game_main_menu():
-    MAIN_MENU = True
-    button = ButtonWithText(0, 0, 100, 50,
-                            background_color_for_buttons, "TEST", (0, 0, 0), 10, 10)
-    button2 = ButtonWithImage(
-        100, 100, 64, 64, background_color_for_buttons, "./Resources/cross_icon.png")
-    buttons = [button, button2]
-
-    while MAIN_MENU:
-        screen.fill(screen_color)
-        # for loop for catching evvents for PyGame
-        mouse_position = pygame.mouse.get_pos()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                MAIN_MENU = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                for each in buttons:
-                    if each.is_over(mouse_position):
-                        each.is_clicked()
-
-        # Checking if buttons are hovered over
-        # and if yes then change background button color
-        for each in buttons:
-            if each.is_over(mouse_position):
-                each.is_hovered()
-            else:
-                each.show_button()
-
-        pygame.display.update()
-
-
-def main_game_loop():
-    pass
-
-
-game_main_menu()
+        screen = pygame.display.set_mode((800, 600))
---
+++ config.py
@@ -0,0 +1,19 @@
+import pygame
+
+# Initializing PyGame
+pygame.init()
+
+# Title and Icon
+pygame.display.set_caption("Tic Tac Toe")
+pygame.display.set_icon(pygame.image.load("./Resources/game_icon.png"))
+
+# screen color (in RGB)
+screen_color = (245, 167, 167)  # very darker blue/purple
+hover_color_for_buttons = (249, 216, 156)
+background_color_for_buttons = (130, 196, 195)
+# Creating the screen
+# screen size (has to be a tuple: width x height)
+screen_width = 800
+screen_height = 600
+screen_size = (screen_width, screen_height)
+screen = pygame.display.set_mode(screen_size)
